f9f985a6c609e7360725c58a03f80f6fc1de6479
Niyom <niyomdedhia33@gmail.com>
2026-01-30 15:30:55 +0530

    File strucutre created and dependencies listed

---
+++ config.py
@@ -0,0 +1,16 @@
+IMAGE_WIDTH = 1280
+IMAGE_HEIGHT = 720
+CAPTURE_INTERVAL_SECONDS = 180
+
+DATASET_DIR = "dataset"
+CAPTURE_DIR = "capture/images"
+ENHANCED_DIR = "lighting_enhancement/output"
+
+MATCH_THRESHOLD = 0.7  #If similarity ≥ 70%, accept the match
+
+ATTENDANCE_OUTPUT = "attendance/attendance.json"
+
+
+
+
+
